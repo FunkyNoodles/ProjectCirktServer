ca81dca4a1b0defea32adf3afda2197457f01348
Funky Noodles <nickasds@hotmail.com>
2015-10-23 10:17:52 -0500

    Added gitignore

--- app.js
+++ app.js
@@ -1,47 +1,100 @@
 var express = require('express');
 var http = require('http');
-var fs = require("fs");
+var fs = require("graceful-fs");
 var xmldoc = require("xmldoc");
 var path = require("path");
+var mkdirp = require('mkdirp');
 var parseString = require('xml2js').parseString;
 var app = express();
 
 const PORT = 8080;
 
+var rootURL = 'http://courses.illinois.edu/cisapp/explorer/schedule.xml';
+var baseURL = 'http://courses.illinois.edu/cisapp/explorer';
+
 app.get('/', function (req, req) {
     req.send("This local host is for testing Node JS");
 });
 
-var xml = 5;
-xml = fs.readFile(__dirname + '/schedule.xml', 'utf8', function (err, data) {
-    if (err) throw err;
-    //console.log(data.toString());
-});
+var xml;
 
 xml = (fs.readFileSync("./schedule.xml"));
 xml = xml.toString();
 //console.log(xml.toString());
-newXML(xml);
+newXML(xml, "calendarYears");
 
 var server = app.listen(PORT, function () {
     console.log("\n\nServer listening on: http://localhost:%s", PORT);
 });
 
-function downloadXML(fileString) {
-    var rootURL = 'http://courses.illinois.edu/cisapp/explorer/schedule.xml';
-    var fileString = rootURL.substr(rootURL.indexOf('schedule'));
+function downloadXML(urlString) {
+    var fileString = urlString.substr(urlString.indexOf('schedule'));
+    fileString = "./" + fileString;
+    var slashCount = (fileString.match(/\//ig) || []).length;
+    var dir = "./" + urlString.substring(urlString.indexOf('schedule'), urlString.lastIndexOf('/'));
+    console.log(fileString + "  " + dir + "   " + slashCount);
+    //console.log(baseURL + fileString.substr(1));
+    mkdirp(dir, function (err) {
+    });
     var file = fs.createWriteStream(fileString);
-    http.get(rootURL, function (response) {
+    http.get(urlString, function (response) {
         response.pipe(file);
     });
+    var xmlParsed;
+
+    file.on('finish', function(){
+        //file.end();
+        //xmlParsed = fs.readFileSync(fileString);
+    });
+
+    fs.readFile(fileString, function (err, data) {
+        if (err) {
+            return console.error(err);
+        }
+
+        setTimeout(function () {
+            xmlParsed = data.toString();
+        }, 1000);
+    });
+
+    setTimeout(function () {
+        if (xmlParsed == null) {
+            return;
+        }
+        switch (slashCount) {
+            case 2:
+                newXML(xmlParsed, "terms");
+                break;
+            case 3:
+                newXML(xmlParsed, "subjects");
+                break;
+            case 4:
+                newXML(xmlParsed, "courses");
+                break;
+            case 5:
+                newXML(xmlParsed, "sections");
+                break;
+            default:
+                console.log("Errrrrrrrrrrrrrrrrrrrrrrr");
+        }
+    }, 1000);
 }
 
-function newXML(xml) {
-    ///var newLink = attr;
+function newXML(xml,tagName) {
     var document = new xmldoc.XmlDocument(xml);
-    //var newLink = document.calendarYearsNode.valueWithPath("calendarYear@href");
-    //var newLink = document.calendarYearsNode.children[0].attr.href;
-    for (i = 0; i < document.childNamed("calendarYears").children.length; i++) {
-        console.log(document.childNamed("calendarYears").children[i].attr.href);
+    var urlString;
+    for (i = 0; i < document.childNamed(tagName).children.length; i++) {
+        urlString = document.childNamed(tagName).children[i].attr.href;
+        setTimeout(callDownload(urlString), 1000);
     }
+}
+
+function callDownload(urlString) {
+    //console.log("Hit " + urlString);
+    downloadXML(urlString);
+}
+
+function callReadFile (fileString){
+    var xmlParsed = fs.readFileSync(fileString);
+    return xmlParsed;
 }
